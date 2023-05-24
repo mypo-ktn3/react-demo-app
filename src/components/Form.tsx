76704b8029686c907eed56198248540f0d84e9ca
Shota Hira <myrwuver26@gmail.com>
2023-05-25 00:11:16 +0900

    [add] weatherapiの雛形実装

---
+++ src/components/Form.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=4bb3ce0ee4aa4a949e1145116232405&q=London&aqi=no").then(res => res.json()).then(data => console.log(data))
+  }
+  return (
+    <form>
+      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
